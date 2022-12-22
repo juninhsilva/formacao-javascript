29831f2b0f914c3a47bc23342d5b31faa167576c
Gilmar Pereira da Silva Júnior <juninhsilva@live.com>
2022-12-21 23:00:16 -0300

    first commit

---
+++ media-final.js
@@ -0,0 +1,11 @@
+let media = gets();
+
+if (media < 5) {
+  print('REP');
+}
+// TODO: Criar demais condições para a resolução deste desafio.
+else if(media>=5 && media<7){
+    print('REC');
+}else{
+    print('APR');
+}
