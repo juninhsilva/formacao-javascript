c70a04381ce7e9fe59ada5254da29b2634996cdf
Gilmar Pereira da Silva Júnior <juninhsilva@live.com>
2023-06-08 19:34:34 -0300

    ordenacao

--- aumento-salario.js
+++ aumento-salario.js
@@ -62,20 +62,20 @@ let percent;
 
 //TODO: Complete os espaços em branco com uma possível solução para o problema:
 
-if (                   ) {
+if (salary<=400.00) {
   percent = 15;
-} else if (               ) {
+} else if (salary>=400.01 && salary<=800.00) {
   percent = 12;
-} else if (                  ) {
+} else if (salary>=800.01 && salary<=1200.00) {
   percent = 10;
-} else if (         ) {
+} else if (salary>=1200.01 && salary<=2000.00) {
   percent = 7;
 } else {
   percent = 4;
 }
 
-const reajust = (              ) / 100;
-const newSalary =            ;
+const reajust = (salary*percent) / 100;
+const newSalary =salary+reajust;
 
 print("Novo salario: " + newSalary.toFixed(2));
 print("Reajuste ganho: " + reajust.toFixed(2));
